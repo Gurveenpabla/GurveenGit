87f5bbf0790beebb4cbcc07c9f8f2413419b7c2e
vmsoGroup3 <soteched2017+group3@gmail.com>
2020-10-02 21:20:29 +0000

    change line 3

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("gurveenwazhere"); 
+    System.out.println("gurveenwazheree"); 
   }
-}  
+}   
+
+/*
+hello
+its not letting me type on the command line..
+
+
+ur email.. is it the one that's on your github? okayy hmm.. yeah it needs to be the one on your account yessss let me try now 
+
+im pretty sure let me just check yeah let me change it 
+
+*/
