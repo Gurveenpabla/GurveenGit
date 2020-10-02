ae3a661332ef75cf0605d9c5a168711f79aa5a17
NS2329 <n.sharma2329@gmail.com>
2020-10-02 21:37:22 +0000

    enter message

--- Main.java
+++ Main.java
@@ -7,9 +7,12 @@ class Main {
 /*
 
 now we make a change for eg we are adding these comments right so this is our change...
-now gurveen type git add . 
+now gurveen type git add .  WAIT you need to config your stuff first-
+ok no quotes that's the email on ur github right? okay now config again but without global no dashes yesss
+
+now follow the steps
+
+yes 
 
 soo...
-
 */
-
